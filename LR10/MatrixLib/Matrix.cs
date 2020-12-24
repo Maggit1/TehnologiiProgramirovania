41ca61d4b50c1dc55427da1ea34d5f8767d7540c
Maggit1 <76495291+Maggit1@users.noreply.github.com>
2020-12-24 06:29:28 +0300

    Add files via upload

---
+++ LR10/MatrixLib/Matrix.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.IO;
+
+namespace MatrixLib
+{
+    public class Matrix
+    {
+        private float[,] MX;
+        int m, n;
+
+        public void GenerateMatrix(int M, int N)
+        {
+            m = M; n = N;
+
+            Random r = new Random(DateTime.Now.Millisecond);
+
+            MX = new float[M, N];
+
+            for (int i = 0; i < M; i++)
+                for (int j = 0; j < N; j++)
+                    MX[i, j] = (float)r.Next(1000);
+        }
+
+        public void SaveMatrix(string pFileName)
+        {
+            if (MX.Length > 0)
+            {
+                if (File.Exists(pFileName))
+                    File.Delete(pFileName);
+                FileInfo f = new FileInfo(pFileName);
+                TextWriter tw = f.CreateText();
+
+                tw.WriteLine(m.ToString());
+                tw.WriteLine(n.ToString());
+
+                for (int i = 0; i < m; i++)
+                    for (int j = 0; j < n; j++)
+                        tw.WriteLine(i.ToString() + " " + j.ToString() + " " + MX[i, j].ToString("E10"));
+                tw.Close();
+            }
+        }
+
+        public Boolean LoadMatrix(string pFileName)
+        {
+            if (File.Exists(pFileName))
+            {
+                try
+                {
+                    TextReader tr = File.OpenText(pFileName);
+                    m = Convert.ToInt32(tr.ReadLine());
+                    n = Convert.ToInt32(tr.ReadLine());
+
+                    MX = new float[m, n];
+                    string line;
+                    string[] substring;
+
+                    for (int i = 0; i < m; i++)
+                        for (int j = 0; j < n; j++)
+                        {
+                            line = tr.ReadLine();
+                            substring = line.Split(new char[] { ' ' }, 3);
+                            MX[i, j] = Convert.ToSingle(substring[2]);
+                        }
+                    tr.Close();
+
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+
+        public void PrintMatrix()
+        {
+            if (MX.Length > 0)
+            {
+                Console.WriteLine("\n");
+                for (int i = 0; i < m; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        Console.Write(MX[i, j].ToString("E3") + " ");
+                    }
+                    Console.WriteLine();
+                }
+
+            }
+        }
+
+        public float GetSumOfEven
+        {
+            get
+            {
+                float SumOfEven = 0;
+                for (int i = 0; i < m; i++)
+                    for (int j = 0; j < n; j++)
+                    {
+                        if ((i + j) % 2 == 0)
+                            SumOfEven += MX[i, j];
+                    }
+                return SumOfEven;
+            }
+        }
+    }
+}
---
+++ LR10/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using MatrixLib;
+
+namespace LW_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Clear();
+            Console.Title = ("Лабораторная работа №10");
+
+            Matrix m1 = new Matrix();
+            Matrix m2 = new Matrix();
+
+            m1.GenerateMatrix(9, 3);
+            m1.SaveMatrix("MX1.txt");
+            
+            m2.GenerateMatrix(6, 5);
+            m2.SaveMatrix("MX2.txt");
+                                                
+            if (m1.LoadMatrix("MX1.txt"))
+                m1.PrintMatrix();
+            
+            Console.WriteLine("\n\n");
+            
+            if (m2.LoadMatrix("MX2.txt"))
+                m2.PrintMatrix();
+            
+            Console.WriteLine("\n\n");
+
+            float sum;
+            sum = m1.GetSumOfEven + m2.GetSumOfEven;
+            
+            Console.WriteLine
+                ("Сумма элементов с чётной суммой индексов в двух матрицах = " 
+                + sum.ToString("E3"));
+
+            Console.ReadKey();
+        }
+    }
+}
