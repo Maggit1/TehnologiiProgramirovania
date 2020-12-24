1a6850fcaf46df64ff2db7bd563607307848c0a3
Maggit1 <76495291+Maggit1@users.noreply.github.com>
2020-12-24 06:27:23 +0300

    Add files via upload

---
+++ LR7/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LW_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if !DEBUG
+            TextWriter save_out = Console.Out;
+            TextReader save_in = Console.In;
+            var new_out = new StreamWriter(@"seg_output.txt");
+            var new_in = new StreamReader(@"seg_input.txt");
+            Console.SetOut(new_out);
+            Console.SetIn(new_in);
+#endif
+            Segment s1, s2;
+#if DEBUG 
+            s2 = new Segment(1, 2, 3, 4);
+            s2.Info(ConsoleColor.Red, ConsoleColor.Black);
+#endif
+
+#if !DEBUG
+            s1 = Segment.CreateSegmentFromFile();
+            s1.Info();
+            s2 = new Segment(5, 6, 7, 8);
+            s2.Info();
+#endif
+
+#if !DEBUG
+            Console.SetOut(save_out); new_out.Close();
+            Console.SetIn(save_in); new_in.Close();
+#endif
+
+#if DEBUG
+            Console.ReadKey();
+#endif
+        }
+    }
+}
---
+++ LR7/Segment.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LW_7
+{
+    class Segment
+    {
+        private double x0, y0, x, y;
+
+        private Segment() { }
+
+        public Segment(double sX0, double sY0, double sX, double sY)
+        {
+            x0 = sX0; y0 = sY0; x = sX; y = sY;
+        }
+        
+        private void Load()
+        {
+            x0 = Convert.ToDouble(Console.ReadLine());
+            y0 = Convert.ToDouble(Console.ReadLine());
+            x = Convert.ToDouble(Console.ReadLine());
+            y = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public static Segment CreateSegmentFromFile()
+        {
+            double xx0 = Convert.ToDouble(Console.ReadLine());
+            double yy0 = Convert.ToDouble(Console.ReadLine());
+            double xx = Convert.ToDouble(Console.ReadLine());
+            double yy = Convert.ToDouble(Console.ReadLine());
+            return new Segment(xx0, yy0, xx, yy);
+        }
+        
+        public double GetLength() { return Math.Sqrt(Math.Pow((x - x0), 2) + Math.Pow((y - y0), 2)); }
+
+        public double GetDistanceToStart() { return Math.Sqrt(Math.Pow((x0), 2) + Math.Pow((y0), 2)); }
+
+        public double GetDistanceToEnd() { return Math.Sqrt(Math.Pow((x), 2) + Math.Pow((y), 2)); }
+
+        public void Info()
+        {
+            String str = "\n*       Отрезок в пространстве       *";
+            Console.WriteLine(str);
+            
+            Console.WriteLine(string.Format("x0 = {0:0.00}", x0));
+            Console.WriteLine(string.Format("y0 = {0:0.00}", y0));
+            Console.WriteLine(string.Format("x = {0:0.00}", x));
+            Console.WriteLine(string.Format("y = {0:0.00}", y));
+
+            Console.WriteLine(string.Format("Длина отрезка = {0:0.00}", GetLength()));
+            Console.WriteLine(string.Format("Расстояние от начала координат до начала отрезка = {0:0.00}", GetDistanceToStart()));
+            Console.WriteLine(string.Format("Расстояние от начала координат до конца отрезка = {0:0.00}", GetDistanceToEnd()));
+        }
+
+        public void Info(ConsoleColor fg, ConsoleColor bgc)
+        {
+            Console.ForegroundColor = fg;
+            Console.BackgroundColor = bgc;
+            Console.Clear();
+            Info();
+        }
+    }
+}
